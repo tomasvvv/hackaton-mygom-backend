e365ec7709d634724e9a5aca10f31455d82ba6b4
Tomas Vosylius <tomas.vosylius@mygom.tech>
2021-10-07 13:19:13 +0300

    remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-(0, server_1.create)().then(() => (0, server_1.start)()).catch(error => console.log(error));
-//# sourceMappingURL=index.js.map
